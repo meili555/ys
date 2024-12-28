f8d432a70417f1996772946a844ea21d45a890a7
meili555 <135024085+meili555@users.noreply.github.com>
2024-12-28 23:08:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'k439.endl.site'
+      url.hostname = 'm.k439.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
