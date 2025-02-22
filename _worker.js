e691024b7022f8378d9d7a63238b82c82f185064
meili555 <135024085+meili555@users.noreply.github.com>
2025-02-22 16:07:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'dianle.d954.tk'
+      url.hostname = 'ys.954.tk'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
